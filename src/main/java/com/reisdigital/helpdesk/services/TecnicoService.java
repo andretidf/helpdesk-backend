2f1f478d34554e9129ff2bf90acc5034a1d09929
Andre Luiz Muniz dos Reis <andretidf@gmail.com>
2021-12-15 11:44:24 -0300

    endpoint findAll para Tecnico

--- src/main/java/com/reisdigital/helpdesk/services/TecnicoService.java
+++ src/main/java/com/reisdigital/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.reisdigital.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,9 @@ public class TecnicoService {
 		Optional<Tecnico> obj = repository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 }
